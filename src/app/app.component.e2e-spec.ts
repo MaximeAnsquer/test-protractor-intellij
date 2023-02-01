fba5da3024093e8604b896df1e6dbf6a94a2d9eb
MaximeAnsquer <mansquer@raileurope.com>
2023-02-01 16:24:45 +0100

    First commit

---
+++ src/app/app.component.e2e-spec.ts
@@ -0,0 +1,7 @@
+import {browser} from 'protractor';
+
+describe('Protractor test', function() {
+  it('should pass', function() {
+    browser.get('http://localhost:4200/')
+  });
+});
---
+++ src/app/protractor.conf.ts
@@ -0,0 +1,5 @@
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4200',
+  specs: ['spec.js']
+}
